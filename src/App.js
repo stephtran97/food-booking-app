afebe3654e88f01639919dd3822039ff45986e8b
ThinhTD <tranducthinh.97@gmail.com>
2023-10-10 22:59:00 +0700

    Update body style when modal is open

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Meals from "./components/Meals/Meals";
 import CartProvider from "./store/CartProvider";
 import Cart from "./components/Cart/Cart";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
 
@@ -14,19 +14,24 @@ function App() {
   const hideCartHandler = () => {
     setCartIsShown(false);
   };
+
+  // Prevent body scroll when modal is open
+  useEffect(() => {
+    cartIsShown && (document.body.style.overflow = "hidden");
+    return () => (document.body.style.overflow = "unset");
+  }, [cartIsShown]);
+
   return (
-    <>
-      <CartProvider>
-        {cartIsShown && <Cart onHideCart={hideCartHandler}></Cart>}
-        <Header
-          onShowCart={showCartHandler}
-          onHideCart={hideCartHandler}
-        ></Header>
-        <main>
-          <Meals></Meals>
-        </main>
-      </CartProvider>
-    </>
+    <CartProvider>
+      {cartIsShown && <Cart onHideCart={hideCartHandler}></Cart>}
+      <Header
+        onShowCart={showCartHandler}
+        onHideCart={hideCartHandler}
+      ></Header>
+      <main>
+        <Meals></Meals>
+      </main>
+    </CartProvider>
   );
 }
 
